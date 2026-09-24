1938079214e0d3226bfed18b84149b83fcc46052
dolodorsey <dolodorsey@gmail.com>
2026-09-24 18:25:23 -0400

    Route MAGA alias to full website

--- src/app/make-atl-great-again/page.jsx
+++ src/app/make-atl-great-again/page.jsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function BrandRoute() {
-  redirect('/shop?brand=make-atlanta-great-again');
+  redirect('https://thaoldatlanta.com');
 }
