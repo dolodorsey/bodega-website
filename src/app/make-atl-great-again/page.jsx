61641e4d67632e088961f192b04f82d92076697d
dolodorsey <dolodorsey@gmail.com>
2026-09-24 17:56:16 -0400

    Fix clean BODEGA brand route

--- src/app/make-atl-great-again/page.jsx
+++ src/app/make-atl-great-again/page.jsx
@@ -1,1 +1,5 @@
-import { redirect } from 'next/navigation';\n\nexport default function BrandRoute() {\n  redirect('/shop?brand=make-atlanta-great-again');\n}\n
+import { redirect } from 'next/navigation';
+
+export default function BrandRoute() {
+  redirect('/shop?brand=make-atlanta-great-again');
+}
